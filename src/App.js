bf697088a321fb0aceb7c84cb7b612e1dc97b331
Gustavo Diogenes de Souza <gustavodio1999@gmail.com>
2023-11-22 15:51:40 -0300

    build 1

--- src/App.js
+++ src/App.js
@@ -3,26 +3,47 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { sortear } from "./util/sortear";
 import { content } from "./util/content";
-
+import CustomModal from "./components/Modal";
 
 const App = () => {
-
   const cardsSortidos = sortear([...content]);
   const [score, setScore] = useState(0);
   const [cards, setCards] = useState(cardsSortidos);
   const [indicesVirados, setindicesVirados] = useState([]);
   const [parearCards, setparearCards] = useState([]);
   const [start, setStart] = useState(true);
+  const [life, setLife] = useState(5);
+  const [showModal, setShowModal] = useState(false);
+  const [body, setBody] = useState("");
+  const [button, setButton] = useState("");
+  const handleShow = () => setShowModal(true);
+  const handleClose = () => setShowModal(false);
 
   useEffect(() => {
     setTimeout(() => {
       setStart(false);
-    }, 2000);
+    }, 5000);
   }, []);
+
+  useEffect(() => {
+    if(score === content.length/2){
+      setBody(`Você Ganhou!, Sua Pontuação: ${score}`);
+      setButton("Tentar Novamente");
+      handleShow();
+    }
+  }, [score]);
 
   useEffect(() => {
     setTimeout(match, 1000);
   }, [indicesVirados, cards, parearCards]);
+
+  useEffect(() => {
+    if (life <= 0) {
+      setBody(`Você Perdeu, Sua Pontuação: ${score}`);
+      setButton("Tentar Novamente");
+      handleShow();
+    }
+  }, [life]);
 
   const match = () => {
     if (indicesVirados.length === 2) {
@@ -34,6 +55,8 @@ const App = () => {
         setScore((score) => score + 1);
         setparearCards([...parearCards, primeiroCard.id, segundoCard.id]);
         console.log(parearCards);
+      } else {
+        setLife((life) => life - 1);
       }
       setindicesVirados([]);
     }
@@ -60,7 +83,10 @@ const App = () => {
       </header>
 
       <div className="card-container">
-        <div className="score">Pontuação: {score}</div>
+        <div className="score">
+          Chances: {life} <br />
+          Pontuação: {score}
+        </div>
         {cards.map((card, index) => (
           <div
             key={index}
@@ -73,7 +99,9 @@ const App = () => {
             }`}
             onClick={() => handleCardClick(index)}
           >
-            {indicesVirados.includes(index) || parearCards.includes(card.id) || start
+            {indicesVirados.includes(index) ||
+            parearCards.includes(card.id) ||
+            start
               ? card.texto
               : card.tipo === "res"
               ? "DEFINIÇÃO"
@@ -91,6 +119,13 @@ const App = () => {
         </p>
         <p class="footer-subject">Matéria: Interfaces para Aplicações Web</p>
       </footer>
+      <CustomModal
+        showModal={showModal}
+        handleClose={handleClose}
+        handleShow={handleShow}
+        bodyContent={body}
+        buttonContent={button}
+      />
     </div>
   );
 };
